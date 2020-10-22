b27cf6546c4f0b4121af0a8969960db8f38bca5d
Fábio junik <fabiojunik@gmail.com>
2020-10-22 15:07:25 +0100

    Acrescentada uma mensagem of good morning

--- ola mundo/Program.cs
+++ ola mundo/Program.cs
@@ -7,6 +7,7 @@ namespace ola_mundo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning!");
         }
     }
 }
