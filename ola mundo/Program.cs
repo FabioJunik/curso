9f82a22ffd90dc4d85ca0b802e60fe654ccd7c5f
Fábio junik <fabiojunik@gmail.com>
2020-10-22 15:37:16 +0100

    Acrescentado uma pergunta de How are you today

--- ola mundo/Program.cs
+++ ola mundo/Program.cs
@@ -8,6 +8,7 @@ namespace ola_mundo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning!");
+            Console.WriteLine("How are you today?");
         }
     }
 }
